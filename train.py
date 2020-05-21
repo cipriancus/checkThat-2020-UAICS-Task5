88b17a11235e645e79be32d41e794de2c5864ebe
v-cicusm <v-cicusm@microsoft.com>
2020-05-21 21:35:17 +0300

    removed mlp

--- train.py
+++ train.py
@@ -3,7 +3,6 @@ from pyspark.sql import SQLContext
 from algorithms import LR as lr
 from algorithms import DT as dt
 from algorithms import NB as bn
-from algorithms import MLP as mlp
 import utils.BuildDataset as buildDataset
 import utils.ClassifyAlgorithm as clsf
 import warnings
@@ -26,30 +25,24 @@ sqlContext = SQLContext(spark_context)
 
 train = buildDataset.read_dataset(sqlContext)
 
-# print('---- 1 -----')
-# print('Bayes Naive')
-# bn_classifier = bn.BN(train)
-# clsf.validate(bn_classifier, sqlContext, 'bn')
-# #clsf.classify(bn_classifier, sqlContext, 'bn')
-# del bn_classifier
-#
-# print('---- 2 -----')
-# print('Logistic Regression')
-# lr_classifier = lr.LR(train)
-# clsf.validate(lr_classifier, sqlContext, 'lr')
-# #clsf.classify(lr_classifier, sqlContext, 'lr')
-# del lr_classifier
-#
-# print('---- 3 -----')
-# print('DT')
-# dt_alg = dt.DT(train)
-# clsf.validate(dt_alg, sqlContext, 'dt')
-# #clsf.classify(dt_alg, sqlContext, 'dt')
-# del dt_alg
+print('---- 1 -----')
+print('Bayes Naive')
+bn_classifier = bn.BN(train)
+clsf.validate(bn_classifier, sqlContext, 'bn')
+#clsf.classify(bn_classifier, sqlContext, 'bn')
+del bn_classifier
 
-print('---- 4 -----')
-print('MLP')
-mlp_alg = mlp.MLP(train)
-clsf.validate(mlp_alg, sqlContext, 'mlp')
-#clsf.classify(mlp_alg, sqlContext, 'mlp')
-del mlp_alg
+print('---- 2 -----')
+print('Logistic Regression')
+lr_classifier = lr.LR(train)
+clsf.validate(lr_classifier, sqlContext, 'lr')
+#clsf.classify(lr_classifier, sqlContext, 'lr')
+del lr_classifier
+
+print('---- 3 -----')
+print('DT')
+dt_alg = dt.DT(train)
+clsf.validate(dt_alg, sqlContext, 'dt')
+#clsf.classify(dt_alg, sqlContext, 'dt')
+del dt_alg
+
